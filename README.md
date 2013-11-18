05af31e4cae71f22cab7cd5251b1c0f6feebab81
Rob Malouf <rmalouf@mail.sdsu.edu>
2013-11-17 20:41:45 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 morphology
 ==========
 
-Resources for Instrumented Item-and-Paradigm morphology
+This project contains resources for doing what Olivier Bonami (2013) calls "Instrumented Item-and-Pattern" morphology.
--- paradigms/entropy.py
+++ paradigms/entropy.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python
 
-import numpy as np
-import pandas as pd
+"""
+This module includes functions for calculating average conditional entropy as
+described in:
+
+    Ackerman, Farrell, James P. Blevins, & Robert Malouf. 2009. "Parts and wholes: 
+        Patterns of relatedness in complex morphological systems and why they matter."
+        In Analogy in Grammar: Form and Acquisition, ed. by James P. Blevins & Juliette 
+        Blevins, 54-82. Oxford: Oxford University Press.
+
+    Ackerman, Farrell and Robert Malouf. 2013. "Morphological organization: The Low 
+      Conditional Entropy Conjecture." Language 89:429-464.
+
+
+Copyright 2013 Rob Malouf
+"""      
+      
+
 import random
 
+import numpy as np
+import pandas as pd
+
 def entropy(paradigm):
+    """Calculate the entropy for each cell in an inflectional paradigm"""
+
     result = pd.Series(index=list(paradigm.columns))
     for cell in paradigm.columns:
         v = paradigm[cell].value_counts(normalize=True)
@@ -12,6 +32,8 @@ def entropy(paradigm):
     return result
     
 def cond_entropy(paradigm):
+    """Calculate the pairwise conditional entropy cells in an inflectional paradigm"""
+
     result = pd.DataFrame(index=paradigm.columns,columns=paradigm.columns)
     for r in paradigm.columns:
         v = paradigm[r].value_counts(normalize=True)
@@ -22,7 +44,10 @@ def cond_entropy(paradigm):
                 result[c][r] = -sum(v * np.log2(v)) - H            
     return result
     
-def bootstrap(paradigm,R):
+def bootstrap(paradigm,R=99):
+    """Perform a bootstrap simulation to estimate the average conditional entropy one
+       would expect to find in a given paradigm (absent implicational relations)."""
+
     Hs = [ cond_entropy(paradigm).mean().mean() ]
     p = paradigm.copy()
     for i in xrange(R):
